856a07cdcfc677af821ae04c21f57540a3af6a7c
VladBonciu <vlad.bonciu@gmail.com>
2023-05-17 18:33:47 +0300

    a

--- Assets/Scripts/PlayerAudio.cs
+++ Assets/Scripts/PlayerAudio.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class PlayerAudio : MonoBehaviour
 {
     public AudioSource bandageSound;
     public AudioSource satchelSound;
     public AudioSource stepSound;
+    public PlayerMove playerMove;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +18,17 @@ public class PlayerAudio : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        if(playerMove.moveDirection != Vector3.zero)
+        {
+            if(Input.GetKey(playerMove.sprintKey))
+            {
+                stepSound.pitch = Mathf.Lerp(stepSound.pitch, 2, Time.deltaTime * 1f);
+            }
+            else
+            {
+                stepSound.pitch = Mathf.Lerp(stepSound.pitch, 1, Time.deltaTime * 1f);
+            }
+        }
     }
 }
